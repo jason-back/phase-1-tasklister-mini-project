91e69e4741d9be82a1c2f241959a35c5c876f558
Jason Back <jason.back01@gmail.com>
2023-08-13 18:48:40 -0400

    added functions for todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const taskButton = document.querySelector(
+        "div#main-content input[value= 'Create New Task"
+    );
+    const textInput = document.querySelector("#new-task-description");
+
+    taskButton.addEventListener("click", taskButtonClick);
+    function taskButtonClick(event) {
+        event.preventDefault();
+        const todoItem = createTodoItem(textInput.value);
+        const myTodos = document.querySelector("ul#tasks");
+        myTodos.append(todoItem);
+    }
+    function createTodoItem(text) {
+        const li = document.createElement("li");
+        li.innerHTML = text;
+        return li;
+    }
 });
